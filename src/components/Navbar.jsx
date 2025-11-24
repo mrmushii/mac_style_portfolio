121449a0710f1e4384acbf58a7f6e142bb0a25cc
Mushfiq <mushfiq2808@gmail.com>
2025-11-24 23:32:39 +0600

    first commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,36 @@
+import { navIcons, navLinks } from '#constants'
+import dayjs from 'dayjs'
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <nav>
+      <div>
+        <img src="/images/logo.svg" />
+        <p className='font-bold'>Mushfiq's Portfolio</p>
+
+        <ul>
+          {
+            navLinks.map(({id,name})=>(
+              <li key={id}>
+                <p>{name}</p>
+              </li>
+            ))
+          }
+        </ul>
+      </div>
+      <div>
+        <ul>
+          {navIcons.map(({id,img})=>(
+            <li key={id}>
+              <img src={img} className='icon-hover' />
+            </li>
+          ))}
+        </ul>
+        <time>{dayjs().format('ddd MMM D h:mm A')}</time>
+      </div>
+    </nav>
+  )
+}
+
+export default Navbar
